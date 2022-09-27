ba19894715d65765495f1c2e8595156faaadfdf2
Yimetra <chernyaev.artemiy@mail.ru>
2022-09-27 11:31:37 +0300

    Added ex_4 in pr_1.

---
+++ src/practical_exercises/pr_1/ex_3.java
@@ -0,0 +1,40 @@
+package practical_exercises.pr_1;
+import java.util.Scanner;
+
+public class ex_3 {
+    public static int sum(int[] arr){
+        int sum = 0;
+
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+
+        return sum;
+    }
+
+    public static float mean(int[] arr){
+        return (float)sum(arr) / arr.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+
+        int n;
+
+        n = sc.nextInt();
+
+        System.out.print("Enter array: ");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+                arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Sum  = " + sum(arr));
+        System.out.println("Mean = " + mean(arr));
+
+        sc.close();
+    }
+}
---
+++ src/practical_exercises/pr_1/ex_4.java
@@ -0,0 +1,67 @@
+package practical_exercises.pr_1;
+
+import java.util.Scanner;
+
+public class ex_4 {
+    public static int max(int[] arr){
+        int max = arr[0];
+        int i = 0;
+
+        while(i < arr.length){
+            if(arr[i] > max) max = arr[i];
+            i++;
+        }
+
+        return max;
+    }
+
+    public static int min(int[] arr){
+        int min = arr[0];
+        int i = 0;
+
+        while(i < arr.length){
+            if(arr[i] < min) min = arr[i];
+            i++;
+        }
+
+        return min;
+    }
+
+    public static int sum(int[] arr){
+        int sum = 0;
+        int i = 0;
+
+        while(i < arr.length){
+            sum += arr[i];
+            i++;
+        }
+
+        return sum;
+    }
+
+    public static float mean(int[] arr){
+        return (float)sum(arr) / arr.length;
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter array size: ");
+
+        int n;
+        n = sc.nextInt();
+
+        System.out.println("Enter array: ");
+
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Sum  = " + sum(arr));
+        System.out.println("Min = " + min(arr));
+        System.out.println("Max = " + max(arr));
+
+        sc.close();
+    }
+}
