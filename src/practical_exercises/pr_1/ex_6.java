3f366ec15f141fb4842e4ae42a8378aca1dd94d3
ArtemiyChernyaev <chernyaev.artemiy@mail.ru>
2022-09-27 15:15:09 +0300

    Completed pr_1.

---
+++ src/practical_exercises/pr_1/ex_6.java
@@ -0,0 +1,6 @@
+public class ex_6 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; ++i)
+            System.out.println(String.format("%d) %5.5f", i,  1.0 / i));
+    }
+}
---
+++ src/practical_exercises/pr_1/ex_7.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ex_7 {
+    public static void main(String[] args) {
+        System.out.println("Enter number to factorization:");
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int res = 1;
+
+        for (int i = 1; i <= n; ++i)
+            res *= i;
+
+        System.out.println("Answer:");
+        System.out.println(res);
+    }
+}
