793da53c226c3340badc9729f6a3df3e7446d00c
Yimetra <chernyaev.artemiy@mail.ru>
2022-11-26 16:51:45 +0300

    Added pr_11.

---
+++ src/practical_exercises/pr_11/SortTime.java
@@ -0,0 +1,12 @@
+import java.util.concurrent.TimeUnit;
+
+public class SortTime {
+    public static void main(String[] args) {
+
+        long start = System.nanoTime();
+
+        long end = System.nanoTime();
+        long timeInMillis = TimeUnit.MILLISECONDS.convert(end - start, TimeUnit.NANOSECONDS);
+        System.out.println("Time spend in ms: " + timeInMillis);
+    }
+}
